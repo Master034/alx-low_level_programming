1b5a357155c7d92ddf117d03089d110d4153a29c
Allan Kinzi <kinziallan09@gmail.com>
2023-04-17 08:12:33 -0700

    Struct Topic Initial Commit

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include "dog.h"
+
+/**
+ * init_dog - initializes struct dog
+ * @d: pointer to struct
+ * @name: struct name parameter
+ * @age: age parameter
+ * @owner: owner parameter
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	if (d == NULL)
+	{
+		d = malloc(sizeof(struct dog));
+	}
+
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
+}
