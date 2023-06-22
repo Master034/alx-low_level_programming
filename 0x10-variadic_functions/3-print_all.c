905bfbf67a62ea380006f4fc8d5f50424b604dfd
Allan Kinzi <kinziallan09@gmail.com>
2023-06-22 12:09:40 -0700

    Fix bugs

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -12,7 +12,7 @@ void print_all(const char * const format, ...)
 {
 	unsigned int i;
 	va_list args;
-	char *s, *separator;
+	char *j, *separator;
 
 	va_start(args, format);
 
@@ -21,18 +21,26 @@ void print_all(const char * const format, ...)
 	i = 0;
 	while (format && format[i])
 	{
-		if (format[i] == 'c')
-			printf("%s%c", separator,  va_arg(args, int));
-		else if (format[i] == 'i')
-			printf("%s%d", separator, va_arg(args, int));
-		else if (format[i] == 'f')
-			printf("%s%f", separator, va_arg(args, double));
-		else if (format[i] == 's')
+		switch (format[i])
 		{
-			s = va_arg(args, char *);
-			if (s == NULL)
-				s = "(nil)";
-			printf("%s%s", separator, s);
+			case 'c':
+				printf("%s%c", separator,  va_arg(args, int));
+				break;
+			case 'i':
+				printf("%s%d", separator, va_arg(args, int));
+				break;
+			case 'f':
+				printf("%s%f", separator, va_arg(args, double));
+				break;
+			case 's':
+				j = va_arg(args, char *);
+				if (j == NULL)
+					j = "(nil)";
+				printf("%s%s", separator, j);
+				break;
+			default:
+				i++;
+				continue;
 		}
 		separator = ", ";
 		i++;
