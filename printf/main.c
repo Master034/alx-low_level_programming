e9f27859183cd7924fece72fcc5ec034f2fb3be1
Allan Kinzi <112064203+Master034@users.noreply.github.com>
2023-06-27 21:41:55 +0300

    Update main.c Test Length

--- printf/main.c
+++ printf/main.c
@@ -5,6 +5,8 @@ int main()
 	void *ptr = malloc(sizeof(int));
 	unsigned int b = 98;
 	int num = 101;
+	short int h_value = 456;
+	long int l_value = 789;
 
 	_printf("Hello, %s! Today is %c. and %b \n", "John", 'S',b);
 	_printf("Octal %o \n", b);
@@ -14,6 +16,9 @@ int main()
 	_printf("%S\n", "Best\nSchool");
 	_printf("Pointer value: %p\n", ptr);
 	_printf("Number with '+' flag: %+d\n", num);
+	_printf("Long Integer (%%ld): %ld\n", l_value);
+	_printf("Long Unsigned Integer (%%lu): %lu\n", l_value);
+	_printf("Short Integer (%%hi): %hi\n", h_value);
 	free(ptr);
 	return (0);
 }
