0186173734274fc7f872d326eec25fab0a4beba6
Allan Kinzi <112064203+Master034@users.noreply.github.com>
2023-07-19 10:40:54 +0300

    Update add2.c Format Variable Declaration

--- Shell_Test/add2.c
+++ Shell_Test/add2.c
@@ -2,15 +2,16 @@
 #include <stdlib.h>
 
 int main(int argc, char *argv[]) {
+    int num1, num2, sum;
     if (argc != 3) {
         printf("Usage: %s <num1> <num2>\n", argv[0]);
         return 1;
     }
 
-    int num1 = atoi(argv[1]);
-    int num2 = atoi(argv[2]);
+    num1 = atoi(argv[1]);
+    num2 = atoi(argv[2]);
 
-    int sum = num1 + num2;
+    sum = num1 + num2;
     printf("The sum of %d and %d is: %d\n", num1, num2, sum);
 
     return 0;
