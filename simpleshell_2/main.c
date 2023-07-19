cb9fbc5c53ede30a948b1c695494a6392effa980
Allan Kinzi <112064203+Master034@users.noreply.github.com>
2023-07-19 09:37:40 +0300

    Update main.c

--- simpleshell_2/main.c
+++ simpleshell_2/main.c
@@ -1,46 +1,46 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
-
-#define BUFFER_SIZE 1024
+#include "shell.h"
 
 int main(void) {
     char *command = NULL;
     size_t bufsize = 0;
     ssize_t characters;
 
-    while (1) {
+    while (1)
+    {
         printf("simple_shell$ ");
         characters = getline(&command, &bufsize, stdin);
-
-        if (characters == -1) {
-            if (feof(stdin)) {
-                printf("\n");  // Handle end of file condition (Ctrl+D)
+        
+        if (characters == -1)
+        {
+            if (feof(stdin))
+            {
+                printf("\n");
                 break;
             }
             perror("getline");
             exit(EXIT_FAILURE);
         }
-
-        if (characters > 0 && command[characters - 1] == '\n') {
-            command[characters - 1] = '\0';  // Remove the trailing newline character
+        if (characters > 0 && command[characters - 1] == '\n')
+        {
+            command[characters - 1] = '\0';
         }
-
-        // Execute the command
-        if (command[0] != '\0') {
-            if (fork() == 0) {
+        if (command[0] != '\0')
+        {
+            if (fork() == 0)
+            {
                 // Child process
                 char *args[] = {command, NULL};
                 execve(command, args, NULL);
                 perror("execve");
                 exit(EXIT_FAILURE);
-            } else {
+            }
+            else
+            {
                 // Parent process
-                wait(NULL);  // Wait for the child process to complete
+                wait(NULL); 
             }
         }
     }
-
     free(command);
     return 0;
 }
