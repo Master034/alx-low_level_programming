b5e907ef17f1a24c6108e1eb61a5381316c8f130
Allan Kinzi <112064203+Master034@users.noreply.github.com>
2023-07-19 10:01:18 +0300

    Update add.c Fix Variable declaration changes

--- simpleshell_2/add.c
+++ simpleshell_2/add.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 int main(void) {
-    int num1, num2;
+    int num1, num2, sum;
     
     printf("Enter the first number: ");
     scanf("%d", &num1);
@@ -9,7 +9,7 @@ int main(void) {
     printf("Enter the second number: ");
     scanf("%d", &num2);
     
-    int sum = num1 + num2;
+    sum = num1 + num2;
     printf("The sum of %d and %d is: %d\n", num1, num2, sum);
     
     return 0;
