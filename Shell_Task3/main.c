c41958ff3d71806d5663431e5b7ff1ce446828ce
Allan Kinzi <kinziallan09@gmail.com>
2023-07-19 01:22:32 -0700

    Add Shell Task 3

---
+++ Shell_Task3/main.c
@@ -0,0 +1,47 @@
+#include "shell.h"
+
+int main(void) {
+    char *command = NULL;
+    size_t bufsize = 0;
+    ssize_t characters;
+
+    while (1)
+    {
+        printf("simple_shell$ ");
+        characters = getline(&command, &bufsize, stdin);
+        
+        if (characters == -1)
+        {
+            if (feof(stdin))
+            {
+                printf("\n");
+                break;
+            }
+            perror("getline");
+            exit(EXIT_FAILURE);
+        }
+        if (characters > 0 && command[characters - 1] == '\n')
+        {
+            command[characters - 1] = '\0';
+        }
+   
+        if (command[0] != '\0')
+        {
+            char *args[MAX_ARGS];
+            
+            tokenizeCommand(command, args);
+            if (fork() == 0)
+            {
+                execve(args[0], args, NULL);
+                perror("execve");
+                exit(EXIT_FAILURE);
+            }
+            else
+            {
+                wait(NULL);
+            }
+        }
+    }
+    free(command);
+    return 0;
+}
