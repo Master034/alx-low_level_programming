22feed6477b95335fbf7d04da01df6da318ee82d
Allan Kinzi <kinziallan09@gmail.com>
2023-07-20 08:49:45 -0700

    Add Exit Function

---
+++ Shell_Task8/exit.cs
@@ -0,0 +1,24 @@
+#include "shell.h"
+
+/**
+ * _exit-Checks exit status
+ * @args:Array of arguments
+ * Return:Nothing
+ */
+
+void _exit(char *command, char **args) {
+    int exit_status = 0;
+    int i = 0;
+
+    if (args[1] != NULL) {
+        exit_status = atoi(args[1]); // Convert the argument to an integer
+    }
+
+    // Free allocated memory before exiting
+    free(command);
+    for (i ; args[i] != NULL; i++) {
+        free(args[i]);
+    }
+
+    exit(exit_status);
+}
