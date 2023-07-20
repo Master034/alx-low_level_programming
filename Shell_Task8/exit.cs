e3f0d2ff7f35e193967e6563b857cb6cc38c03b9
Allan Kinzi <112064203+Master034@users.noreply.github.com>
2023-07-20 18:52:08 +0300

    Update exit.cs Format File

--- Shell_Task8/exit.cs
+++ Shell_Task8/exit.cs
@@ -5,20 +5,14 @@
  * @args:Array of arguments
  * Return:Nothing
  */
-
 void _exit(char *command, char **args) {
     int exit_status = 0;
     int i = 0;
 
-    if (args[1] != NULL) {
-        exit_status = atoi(args[1]); // Convert the argument to an integer
-    }
-
-    // Free allocated memory before exiting
+    if (args[1] != NULL)
+        exit_status = atoi(args[1]);
     free(command);
-    for (i ; args[i] != NULL; i++) {
+    for (i ; args[i] != NULL; i++)
         free(args[i]);
-    }
-
     exit(exit_status);
 }
