af70cbaa5cc952decd2f04828a9df55f4a351fae
Allan Kinzi <112064203+Master034@users.noreply.github.com>
2023-07-20 15:47:32 +0300

    Update print_env.c Format

--- Shell_Task5/print_env.c
+++ Shell_Task5/print_env.c
@@ -1,9 +1,14 @@
 #include "shell.h"
 
-
-void print_environment() {
-    char **env = environ; // Get the pointer to the environment variables
-    while (*env != NULL) {
+/**
+    
+    
+void print_environment()
+{
+    char **env = environ;
+    
+    while (*env != NULL)
+    {
         printf("%s\n", *env);
         env++;
     }
