001c87fffe508b1a11680a6f4b837fc4a1d41929
Allan Kinzi <112064203+Master034@users.noreply.github.com>
2023-07-20 18:19:00 +0300

    Update _strtok.c  Format File

--- Shell_Task7/_strtok.c
+++ Shell_Task7/_strtok.c
@@ -8,22 +8,24 @@
  */
 char* _strtok(char* str, const char* delim)
 {
-    static char* current_str = NULL;
+    static char* crnt_str = NULL;
+    char* token_start = NULL;
+    char* token_end = NULL;
     static bool end_of_string = false;
     
     if (str != NULL)
-        current_str = str;
-    if (current_str == NULL || end_of_string)
+        crnt_str = str;
+    if (crnt_str == NULL || end_of_string)
         return (NULL);
-    char* token_start = current_str;
-    char* token_end = strpbrk(current_str, delim);
-
+    token_start = crnt_str;
+    token_end = strpbrk(crnt_str, delim);
+    
     if (token_end == NULL)
         end_of_string = true;
     else
     {
         *token_end = '\0';
-        current_str = token_end + 1;
+        crnt_str = token_end + 1;
     }
     return token_start;
 }
