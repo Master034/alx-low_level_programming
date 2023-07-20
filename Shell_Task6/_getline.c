aee2d430fac0592f9be7829af5cc5cf53d664b5c
Allan Kinzi <112064203+Master034@users.noreply.github.com>
2023-07-20 16:18:12 +0300

    Update _getline.c Format

--- Shell_Task6/_getline.c
+++ Shell_Task6/_getline.c
@@ -2,63 +2,44 @@
 
 /**
  * _getline - Gets the line
- * Return: Line Passed in.
+ * Return: Number of Characters
  */
-
-char* _getline() {
-    static char buffer[BUFFER_SIZE];
-    static size_t buffer_pos = 0; // Current position in the buffer
-    static ssize_t chars_in_buffer = 0; // Number of characters in the buffer
-
-    char* line = NULL;
-    size_t line_len = 0;
-    ssize_t chars_read;
-
-    while (1) {
-        // If the buffer is empty, read new data from the input stream
-        if (buffer_pos >= chars_in_buffer) {
-            chars_read = read(STDIN_FILENO, buffer, BUFFER_SIZE);
-            if (chars_read <= 0) {
-                if (line != NULL) {
-                    line[line_len] = '\0'; // Null-terminate the line
-                }
-                return line; // Return NULL on error or end of file
-            }
-            buffer_pos = 0;
-            chars_in_buffer = chars_read;
-        }
-
-        char c = buffer[buffer_pos];
-        buffer_pos++;
-
-        if (c == '\n') {
-            if (line == NULL) {
-                line = (char*)malloc((line_len + 1) * sizeof(char));
-            } else {
-                line = (char*)realloc(line, (line_len + 1) * sizeof(char));
-            }
-
-            if (line == NULL) {
-                perror("Memory allocation failed");
-                exit(EXIT_FAILURE);
-            }
-
-            line[line_len] = '\0'; // Null-terminate the line
-            return line;
-        } else {
-            if (line == NULL) {
-                line = (char*)malloc((line_len + 1) * sizeof(char));
-            } else {
-                line = (char*)realloc(line, (line_len + 1) * sizeof(char));
-            }
-
-            if (line == NULL) {
-                perror("Memory allocation failed");
-                exit(EXIT_FAILURE);
-            }
-
-            line[line_len] = c;
-            line_len++;
-        }
+ssize_t _getline(char **lineptr, size_t *n, FILE *stream)
+{
+    int c;
+    size_t i = 0;
+    
+    if (lineptr == NULL || n == NULL || stream == NULL) {
+        return -1; // Invalid input parameters
     }
+    if (*lineptr == NULL || *n < INITIAL_BUFFER_SIZE) {
+        *lineptr = (char*)malloc(INITIAL_BUFFER_SIZE);
+        if (*lineptr == NULL) {
+            perror("Memory allocation failed");
+            return -1;
+        }
+        *n = INITIAL_BUFFER_SIZE;
+    }
+    while ((c = fgetc(stream)) != EOF) 
+    {
+        if (i >= *n - 1) 
+        { 
+            size_t new_size = *n * 2;
+            char* temp = (char*)realloc(*lineptr, new_size);
+            if (temp == NULL) {
+                perror("Memory reallocation failed");
+                return -1;
+            }
+            *lineptr = temp;
+            *n = new_size;
+        }
+        (*lineptr)[i] = c;
+        i++;
+        if (c == '\n')
+            break;
+    }
+    if (i == 0)
+        return -1;
+    (*lineptr)[i] = '\0'; 
+    return i; 
 }
