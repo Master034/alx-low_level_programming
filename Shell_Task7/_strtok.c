62f699d79493e598f636e803a75b72cbf8a4eacd
Allan Kinzi <112064203+Master034@users.noreply.github.com>
2023-07-20 16:32:33 +0300

    Update _strtok.c Format _strtok()

--- Shell_Task7/_strtok.c
+++ Shell_Task7/_strtok.c
@@ -6,28 +6,27 @@
  * @delim: Delimiter
  * Return: Tokenized array
  */
-char* _strtok(char* str, const char* delim) {
+char* _strtok(char* str, const char* delim)
+{
     static char* current_str = NULL;
     static bool end_of_string = false;
-
-    if (str != NULL) {
+    
+    if (str != NULL)
+    {
         current_str = str;
         end_of_string = false;
     }
-
-    if (current_str == NULL || end_of_string) {
-        return NULL;
-    }
-
+    if (current_str == NULL || end_of_string)
+        return (NULL);
     char* token_start = current_str;
     char* token_end = strpbrk(current_str, delim);
 
-    if (token_end == NULL) {
+    if (token_end == NULL)
         end_of_string = true;
-    } else {
+    else
+    {
         *token_end = '\0';
         current_str = token_end + 1;
     }
-
     return token_start;
 }
