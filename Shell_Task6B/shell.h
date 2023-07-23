8c6f013882176ee8e8661ea12aa50ee003e1e691
Allan Kinzi <112064203+Master034@users.noreply.github.com>
2023-07-23 10:31:35 +0300

    Update shell.h Add Headers

--- Shell_Task6B/shell.h
+++ Shell_Task6B/shell.h
@@ -6,11 +6,32 @@
 #include <string.h>
 #include <unistd.h>
 #include <sys/wait.h>
+#include <stdbool.h> 
 
 #define BUFFER_SIZE 1024
 #define MAX_ARGS 10
+#define INITIAL_BUFFER_SIZE 64
 
 extern char **environ;
+
+/**
+ * struct liststr - list
+ * @num: number field
+ * @str: a string
+ * @next: points to the next node
+ */
+typedef struct liststr
+{
+	int num;
+	char *str;
+	struct liststr *next;
+} list_n;
+
+typedef struct getinfo
+{
+	list_n *env;
+} info_d;
+
 
 void tokenizeCommand(char *command, char **args);
 void print_environment();
