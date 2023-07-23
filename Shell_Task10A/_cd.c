b1e2fa660b2d1cab27776f577aba318aabbaeb4d
Allan Kinzi <112064203+Master034@users.noreply.github.com>
2023-07-23 14:01:54 +0300

    Update _cd.c Add CD funtionality

--- Shell_Task10A/_cd.c
+++ Shell_Task10A/_cd.c
@@ -1,1 +1,54 @@
 #include "shell.h"
+
+/**
+ * cd - Change the current directory of the process.
+ * @path: The directory path to change to (can be NULL for default).
+ */
+void cd(const char *path)
+{
+    if (path == NULL || path[0] == '\0')
+    {
+        path = getenv("HOME");
+        if (path == NULL)
+        {
+            fprintf(stderr, "cd: %s\n", "HOME environment variable not set");
+            return;
+        }
+    }
+
+    char current_path[BUFFER_SIZE];
+    if (getcwd(current_path, BUFFER_SIZE) == NULL)
+    {
+        perror("cd");
+        return;
+    }
+    if (strcmp(path, "-") == 0)
+    {
+        path = getenv("OLDPWD");
+        if (path == NULL)
+        {
+            fprintf(stderr, "cd: %s\n", "OLDPWD not set");
+            return;
+        }
+        printf("%s\n", path);
+    }
+    if (chdir(path) == -1)
+    {
+        perror("cd");
+        return;
+    }
+    char new_path[BUFFER_SIZE];
+    if (getcwd(new_path, BUFFER_SIZE) == NULL)
+    {
+        perror("cd");
+        return;
+    }
+    if (_setenv("PWD", new_path, 1) == -1)
+    {
+        fprintf(stderr, "cd: %s\n", "Failed to update PWD environment variable");
+    }
+    if (_setenv("OLDPWD", current_path, 1) == -1)
+    {
+        fprintf(stderr, "cd: %s\n", "Failed to update OLDPWD environment variable");
+    }
+}
