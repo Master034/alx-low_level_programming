b4883a3855f69d22fde898bf1292551213dff6fe
Allan Kinzi <112064203+Master034@users.noreply.github.com>
2023-07-23 15:10:45 +0300

    Update execute.c Modify Function

--- Shell_Task11A/execute.c
+++ Shell_Task11A/execute.c
@@ -5,40 +5,70 @@ void execute_commands(const char *commands)
     char *commands_copy = strdup(commands);
     char *args[MAX_ARGS];
     char *trimmed_command;
-     int len;
+    char *semicolon_token;
     char *token = NULL;
-    
+     int arg_count = 1, status;
+    char *command;
+    pid_t pid;
+
     if (commands_copy == NULL)
     {
         perror("execute_commands: strdup");
         return;
     }
 
-    token = _strtok(commands_copy, ";");
-    while (token != NULL)
+    semicolon_token = strtok(commands_copy, ";");
+    while (semicolon_token != NULL)
     {
-        trimmed_command = token;
+        trimmed_command = semicolon_token;
         while (*trimmed_command == ' ')
             trimmed_command++;
-        len = strlen(trimmed_command);
+        int len = strlen(trimmed_command);
         while (len > 0 && trimmed_command[len - 1] == ' ')
             trimmed_command[--len] = '\0';
 
         if (trimmed_command[0] != '\0')
         {
-            tokenizeCommand(trimmed_command, args);
+            char *token = _strtok(trimmed_command, " ");
+            command = token;
+            token = _strtok(NULL, " ");
+            while (*command == ' ')
+                command++;
+            int len = strlen(command);
+            while (len > 0 && command[len - 1] == ' ')
+                command[--len] = '\0';
 
-            if (strcmp(args[0], "exit") == 0)
-                _handle_exit(args[0]);
-            else if (strcmp(args[0], "setenv") == 0)
-                _setenv(args[1], args[2], 0);
-            else if (strcmp(args[0], "unsetenv") == 0)
-                unsetenv(args[1]);
-            else if (strcmp(args[0], "cd") == 0)
-                _cd(args[1]);
-            else
+            if (command[0] != '\0')
             {
-                if (fork() == 0)
+                char *args[MAX_ARGS];
+                args[0] = command;
+                int arg_count = 1;
+                while (token != NULL)
+                {
+                    while (*token == ' ')
+                        token++;
+                    len = strlen(token);
+                    while (len > 0 && token[len - 1] == ' ')
+                        token[--len] = '\0';
+
+                    if (token[0] != '\0')
+                    {
+                        args[arg_count++] = token;
+                        if (arg_count >= MAX_ARGS - 1)
+                            break;
+                    }
+
+                    token = _strtok(NULL, " ");
+                }
+
+                args[arg_count] = NULL;
+                pid = fork();
+                if (pid == -1)
+                {
+                    perror("fork");
+                    exit(EXIT_FAILURE);
+                }
+                else if (pid == 0)
                 {
                     execve(args[0], args, environ);
                     perror("execve");
@@ -46,11 +76,12 @@ void execute_commands(const char *commands)
                 }
                 else
                 {
-                    wait(NULL);
+                    waitpid(pid, &status, 0);
                 }
             }
         }
-        token = _strtok(NULL, ";");
+
+        semicolon_token = _strtok(NULL, ";");
     }
 
     free(commands_copy);
