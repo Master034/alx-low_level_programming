2b9edc51846d8505396b646e8ca7d2007898f0f1
Allan Kinzi <112064203+Master034@users.noreply.github.com>
2023-07-23 14:40:03 +0300

    Update main.c Modify Execute commands

--- Shell_Task11A/main.c
+++ Shell_Task11A/main.c
@@ -33,30 +33,7 @@ int main(void) {
             print_environment();
         else if (command[0] != '\0')
         {
-            char *args[MAX_ARGS];
-            
-            tokenizeCommand(command, args);
-            if (strcmp(args[0], "exit") == 0)
-                _handle_exit(args[0]);
-            if (strcmp(args[0], "setenv") == 0)
-                _setenv(args[1], args[2], overwrite);
-            if (strcmp(args[0], "cd") == 0)
-                _cd(args[1]);
-            else if (fork() == 0)
-            {
-                execve(args[0], args, NULL);
-                while (path_token != NULL) {
-                    snprintf(path_command, BUFFER_SIZE, "%s/%s", path_token, args[0]);
-                    execve(path_command, args, environ);
-                    path_token = _strtok(NULL, ":");
-                }
-                perror("execve");
-                exit(EXIT_FAILURE);
-            }
-            else
-            {
-                wait(NULL);
-            }
+            execute_commands(command);
         }
     }
     free(command);
