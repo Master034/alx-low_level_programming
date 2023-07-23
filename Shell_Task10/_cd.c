256e9b2708314111093c4d357071a384986f0b31
Allan Kinzi <112064203+Master034@users.noreply.github.com>
2023-07-23 08:41:42 +0300

    Update _cd.c Update CD

--- Shell_Task10/_cd.c
+++ Shell_Task10/_cd.c
@@ -12,50 +12,71 @@ void update_PWD_env(char *new_path)
 
 int handle_cd_command(char **args)
 {
-    char cwd[BUFFER_SIZE];
+   char cwd[BUFFER_SIZE];
     char current_dir[BUFFER_SIZE] = "";
-    
-    if (args[1] == NULL || strcmp(args[1], "~") == 0) {
+    char prev_cwd[BUFFER_SIZE];
+
+    if (args[1] == NULL || strcmp(args[1], "~") == 0)
+    {
         char *home_dir = getenv("HOME");
-        if (home_dir) {
-            if (chdir(home_dir) != 0) {
+        if (home_dir)
+        {
+            if (chdir(home_dir) != 0)
+            {
                 perror("cd");
                 return -1;
             }
             update_PWD_env(home_dir);
             return 0;
-        } else {
+        }
+        else
+        {
             fprintf(stderr, "cd: HOME not set\n");
             return -1;
         }
     }
-    if (strcmp(args[1], "-") == 0) {
+    if (strcmp(args[1], "-") == 0)
+    {
         char *old_pwd = getenv("OLDPWD");
-        if (old_pwd) {
-            if (chdir(old_pwd) != 0) {
+        if (old_pwd)
+        {
+            if (chdir(old_pwd) != 0)
+            {
                 perror("cd");
                 return -1;
             }
             update_PWD_env(old_pwd);
-            printf("%s\n", old_pwd); 
+            printf("%s\n", old_pwd);
             return 0;
-        } else {
+        }
+        else
+        {
             fprintf(stderr, "cd: OLDPWD not set\n");
             return -1;
         }
     }
-    if (chdir(args[1]) != 0) {
+    if (getcwd(prev_cwd, BUFFER_SIZE) == NULL)
+    {
+        perror("getcwd");
+        return -1;
+    }
+
+    if (chdir(args[1]) != 0)
+    {
         perror("cd");
         return -1;
     }
-    if (getcwd(cwd, BUFFER_SIZE) != NULL) {
+    if (getcwd(cwd, BUFFER_SIZE) != NULL)
+    {
         update_PWD_env(cwd);
         strncpy(current_dir, cwd, BUFFER_SIZE - 1);
         current_dir[BUFFER_SIZE - 1] = '\0';
-    } else {
+    }
+    else
+    {
         perror("cd");
         return -1;
     }
-    printf("%s\n", cwd);
+    printf("%s\n", current_dir);
     return 0;
 }
