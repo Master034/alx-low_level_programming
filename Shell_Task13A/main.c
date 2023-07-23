8caaedc2b2a34b904372590967cda9a5d0474ec3
Allan Kinzi <112064203+Master034@users.noreply.github.com>
2023-07-23 16:41:44 +0300

    Update main.c Handle Alias

--- Shell_Task13A/main.c
+++ Shell_Task13A/main.c
@@ -29,7 +29,32 @@ int main(void) {
             print_environment();
         else if (command[0] != '\0')
         {
-            execute_commands(command);
+            char *args[MAX_ARGS];
+            
+            tokenizeCommand(command, args);
+            if (strcmp(args[0], "exit") == 0)
+                _handle_exit(args[0]);
+            if (strcmp(args[0], "alias") == 0)
+                handle_alias_command(args);
+            if (strcmp(args[0], "setenv") == 0)
+                _setenv(args[1], args[2], overwrite);
+            if (strcmp(args[0], "cd") == 0)
+                _cd(args[1]);
+            else if (fork() == 0)
+            {
+                execve(args[0], args, NULL);
+                while (path_token != NULL) {
+                    snprintf(path_command, BUFFER_SIZE, "%s/%s", path_token, args[0]);
+                    execve(path_command, args, environ);
+                    path_token = _strtok(NULL, ":");
+                }
+                perror("execve");
+                exit(EXIT_FAILURE);
+            }
+            else
+            {
+                wait(NULL);
+            }
         }
     }
     free(command);
