2e3ab998f11b60f2f2e5b50bad35e427162b4e09
Allan Kinzi <112064203+Master034@users.noreply.github.com>
2023-07-23 17:39:52 +0300

    Update shell.h

--- Shell_Task13A/shell.h
+++ Shell_Task13A/shell.h
@@ -35,7 +35,7 @@ void execute_commands(const char *commands);
 void handle_alias_command(char **args);
 void set_alias(const char *name, const char *value);
 void print_alias(const char *name);
-int find_alias(const char *name);
+AliasInfo *find_alias(const char *name)
 void free_alias_list();
 
 #endif
