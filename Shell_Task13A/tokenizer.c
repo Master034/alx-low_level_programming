b8f072859c2b2dbda6d128dadb92f0e4dcd2d3f3
Allan Kinzi <112064203+Master034@users.noreply.github.com>
2023-07-24 20:07:20 +0300

    Update tokenizer.c Add $ check

--- Shell_Task13A/tokenizer.c
+++ Shell_Task13A/tokenizer.c
@@ -1,16 +1,39 @@
 #include "shell.h"
 
+/**
+ * tokenizeCommand - Tokenize a command line input into individual arguments.
+ * @command: The command line input to be tokenized.
+ * @args: A double pointer to an array of strings to store the tokens (arguments).
+ */
 void tokenizeCommand(char *command, char **args)
 {
-  
     int arg_count = 0;
-    char *token = strtok(command, " ");
+    char *token = _strtok(command, " ");
+    char *var_name;
+    char *var_value
     
     while (token != NULL && arg_count < MAX_ARGS - 1)
     {
-        args[arg_count] = token;
+        if (token[0] == '$') 
+        {
+            var_name = token + 1;
+            var_value = getenv(var_name);
+            if (var_value != NULL)
+            {
+                args[arg_count] = var_value;
+            }
+            else
+            {
+                args[arg_count] = token;
+            }
+        }
+        else
+        {
+            args[arg_count] = token;
+        }
+
         arg_count++;
-        token = strtok(NULL, " ");
+        token = _strtok(NULL, " ");
     }
     args[arg_count] = NULL;
 }
