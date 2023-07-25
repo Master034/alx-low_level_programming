07d553fca475abc546b9526ac45416c71eb4734c
Allan Kinzi <112064203+Master034@users.noreply.github.com>
2023-07-25 21:45:33 +0300

    Update str_func1.c Aadd

--- Shell_Task13A/str_func1.c
+++ Shell_Task13A/str_func1.c
@@ -23,7 +23,7 @@ int _strlen(const char *s)
  * Return: Positive byte difference if s1 > s2
  *         0 if s1 = s2
  */
-int _strcmp(char *s1, char *s2)
+int _strcmp(const char *s1, const char *s2)
 {
 	while (*s1 && *s2 && *s1 == *s2)
 	{
