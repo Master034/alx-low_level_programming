d6bf20d8261e160bddd3c8caf5662ea1fd0c868c
Allan Kinzi <112064203+Master034@users.noreply.github.com>
2023-07-25 20:58:36 +0300

    Update str_func1.c Add StrCompare

--- Shell_Task13A/str_func1.c
+++ Shell_Task13A/str_func1.c
@@ -15,3 +15,47 @@ int _strlen(const char *s)
 		;
 	return (len);
 }
+
+/**
+ * _strcmp - Compares two strings.
+ * @s1: The first string to be compared.
+ * @s2: The second string to be compared.
+ * Return: Positive byte difference if s1 > s2
+ *         0 if s1 = s2
+ */
+int _strcmp(char *s1, char *s2)
+{
+	while (*s1 && *s2 && *s1 == *s2)
+	{
+		s1++;
+		s2++;
+	}
+
+	if (*s1 != *s2)
+		return (*s1 - *s2);
+
+	return (0);
+}
+/**
+ * _strncmp - Compare two strings.
+ * @s1: Pointer to a string.
+ * @s2: Pointer to a string.
+ * @n: The first n bytes of the strings to compare.
+ * Return: Less than 0 if s1 is shorter than s2.
+ */
+int _strncmp(const char *s1, const char *s2, size_t n)
+{
+	size_t p;
+
+	for (p = 0; s1[p] && s2[p] && p < n; p++)
+	{
+		if (s1[p] > s2[p])
+			return (s1[p] - s2[p]);
+		else if (s1[p] < s2[p])
+			return (s1[p] - s2[p]);
+	}
+	if (p == n)
+		return (0);
+	else
+		return (-15);
+}
