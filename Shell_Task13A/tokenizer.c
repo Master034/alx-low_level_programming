9bd3343ce21f37771d05233b0162973eb0432bf4
Allan Kinzi <112064203+Master034@users.noreply.github.com>
2023-07-25 22:03:10 +0300

    Update tokenizer.c res

--- Shell_Task13A/tokenizer.c
+++ Shell_Task13A/tokenizer.c
@@ -3,13 +3,13 @@
 void tokenizeCommand(char *command, char **args)
 {
     int arg_count = 0;
-    char *token = strtok(command, " ");
+    char *token = _strtok(command, " ");
     
     while (token != NULL && arg_count < MAX_ARGS - 1)
     {
         args[arg_count] = token;
         arg_count++;
-        token = strtok(NULL, " ");
+        token = _strtok(NULL, " ");
     }
     args[arg_count] = NULL;
 }
