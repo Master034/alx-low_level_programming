94fab3040f69fdd454b717a2ad4d19e62e49b6f9
Master034 <masterdev41@gmail.com>
2023-07-26 09:50:53 -0700

    Add Str_function2

---
+++ Shell_Task13A/str_func2.c
@@ -0,0 +1,28 @@
+#include "shell.h"
+
+/**
+ * _strdup - Duplicates a string.
+ * @str: The string to duplicate.
+ * Return: A pointer to the duplicated string, or NULL if allocation fails.
+ */
+char *_strdup(const char *str)
+{
+	size_t len = _strlen(str), i = 0;
+	char *duplicate = (char *)malloc((len + 1) * sizeof(char));
+	
+	if (str == NULL)
+	{
+		return NULL;
+	}
+	
+	if (duplicate == NULL)
+	{
+		perror("_strdup: malloc");
+		return NULL;
+	}
+	for (i = 0; i <= len; i++)
+	{
+		duplicate[i] = str[i];
+	}
+	return (duplicate);
+}
