4202afe4be5d88f33abe1f8729e7bad640cabb33
Allan Kinzi <112064203+Master034@users.noreply.github.com>
2023-07-26 21:26:59 +0300

    Update main.c REs

--- Shell_Task13A/main.c
+++ Shell_Task13A/main.c
@@ -1,24 +1,25 @@
 #include "shell.h"
 
 /**
- * main - main function.
- * Return: void
+ * main - Main function for the simple shell.
+ * Return: 0 on success.
  */
 int main(void)
 {
-	size_t bufsize = 0;
-	ssize_t characters;
-	char *path_env = getenv("PATH");
-	char *path_token = _strtok(path_env, ":");
-	char *command = NULL;
-	char path_command[BUFFER_SIZE];
-	int overwrite = 0;
-	
-	while (1)
-	{
-		printf("simple_shell$ ");
-		characters = _getline(&command, &bufsize, stdin);
-		if (characters == -1)
+    size_t bufsize = 0;
+    ssize_t characters;
+    char *path_env = getenv("PATH");
+    char *path_token = _strtok(path_env, ":");
+    char *command = NULL;
+    char path_command[BUFFER_SIZE];
+    int overwrite = 0;
+
+    while (1)
+    {
+        printf("simple_shell$ ");
+        characters = _getline(&command, &bufsize, stdin);
+
+        if (characters == -1)
         {
             if (feof(stdin))
             {
@@ -28,41 +29,71 @@ int main(void)
             perror("getline");
             exit(EXIT_FAILURE);
         }
+
         if (characters > 0 && command[characters - 1] == '\n')
             command[characters - 1] = '\0';
-        if (_strcmp(command, "exit") == 0)
-            _handle_exit(command);
-        else if (_strcmp(command, "env") == 0)
-            print_environment();
-        else if (command[0] != '\0')
-        {
-            char *args[MAX_ARGS];
-            
-            tokenizeCommand(command, args);
-            if (_strcmp(args[0], "exit") == 0)
-                _handle_exit(args[0]);
-            else if (_strcmp(args[0], "alias") == 0)
-                handle_alias_command(args);
-            else if (_strcmp(args[0], "setenv") == 0)
-                _setenv(args[1], args[2], overwrite);
-            else if (_strcmp(args[0], "cd") == 0)
-                _cd(args[1]);
-            else if (fork() == 0)
-            {
-                execve(args[0], args, NULL);
-                while (path_token != NULL) {
-                    snprintf(path_command, BUFFER_SIZE, "%s/%s", path_token, args[0]);
-                    execve(path_command, args, environ);
-                    path_token = _strtok(NULL, ":");
-                }
-                perror("execve");
-                exit(EXIT_FAILURE);
-            }
-            else
-                wait(NULL);
-        }
+
+        handle_command(command, &path_token, path_command, overwrite);
+
     }
+
     free_alias_list();
     free(command);
-    return (0);
+    return 0;
+}
+
+/**
+ * handle_command - Handle the entered command.
+ * @command: The command entered by the user.
+ * @path_token: The current token in the PATH environment variable.
+ * @path_command: Buffer to store the command with path.
+ * @overwrite: Flag to indicate whether to overwrite an existing environment variable.
+ * Return: void.
+ */
+void handle_command(char *command, char **path_token, char *path_command, int overwrite)
+{
+    if (_strcmp(command, "exit") == 0)
+        _handle_exit(command);
+    else if (_strcmp(command, "env") == 0)
+        print_environment();
+    else if (command[0] != '\0')
+    {
+        char *args[MAX_ARGS];
+
+        tokenizeCommand(command, args);
+        if (_strcmp(args[0], "exit") == 0)
+            _handle_exit(args[0]);
+        else if (_strcmp(args[0], "alias") == 0)
+            handle_alias_command(args);
+        else if (_strcmp(args[0], "setenv") == 0)
+            _setenv(args[1], args[2], overwrite);
+        else if (_strcmp(args[0], "cd") == 0)
+            _cd(args[1]);
+        else if (fork() == 0)
+        {
+            execute_command(args, path_token, path_command);
+        }
+        else
+            wait(NULL);
+    }
+}
+
+/**
+ * execute_command - Execute the command.
+ * @args: The command and its arguments.
+ * @path_token: The current token in the PATH environment variable.
+ * @path_command: Buffer to store the command with path.
+ * Return: void.
+ */
+void execute_command(char **args, char **path_token, char *path_command)
+{
+    execve(args[0], args, NULL);
+    while (*path_token != NULL)
+    {
+        snprintf(path_command, BUFFER_SIZE, "%s/%s", *path_token, args[0]);
+        execve(path_command, args, environ);
+        *path_token = _strtok(NULL, ":");
+    }
+    perror("execve");
+    exit(EXIT_FAILURE);
 }
