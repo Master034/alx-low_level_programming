ba9080a1835d6d288916b1f56efdef920d7010fd
Allan Kinzi <112064203+Master034@users.noreply.github.com>
2023-07-26 20:04:44 +0300

    Update str_func2.c Remove Constatnt

--- Shell_Task13A/str_func2.c
+++ Shell_Task13A/str_func2.c
@@ -5,7 +5,7 @@
  * @str: The string to duplicate.
  * Return: A pointer to the duplicated string, or NULL if allocation fails.
  */
-const char *_strdup(const char *str)
+char *_strdup(const char *str)
 {
 	size_t len = _strlen(str), i = 0;
 	char *duplicate = (char *)malloc((len + 1) * sizeof(char));
